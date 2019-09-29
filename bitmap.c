282a6f0855a1320c2aaa30c1bf0eed77126302b3
Eshin Kunishima <ek@esh.ink>
2019-09-29 13:41:36 +0900

    format bitmap

--- bitmap.c
+++ bitmap.c
@@ -4,43 +4,45 @@
 #include "bitmap.h"
 
 Bitmap *BitmapNewImage(uint16_t width, uint16_t height) {
-    Bitmap *bitmap;
+  Bitmap *bitmap;
 
-    bitmap = (Bitmap *) calloc(1, sizeof(*bitmap));
+  bitmap = (Bitmap *)calloc(1, sizeof(*bitmap));
 
-    uint32_t headerSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPCOREHEADER);
-    uint32_t rowSize = sizeof(RGBTRIPLE) * width;
-    // Each row in the Pixel array is padded to a multiple of 4 bytes in size.
-    if (rowSize % 4) { rowSize += 4 - rowSize % 4; }
-    uint32_t size = rowSize * height;
+  uint32_t headerSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPCOREHEADER);
+  uint32_t rowSize = sizeof(RGBTRIPLE) * width;
+  // Each row in the Pixel array is padded to a multiple of 4 bytes in size.
+  if (rowSize % 4) {
+    rowSize += 4 - rowSize % 4;
+  }
+  uint32_t size = rowSize * height;
 
-    bitmap->fileHeader = (BITMAPFILEHEADER) {BMP_MAGIC, headerSize + size, 0, 0, headerSize};
-    bitmap->dibHeader = (BITMAPCOREHEADER) {12, width, height, 1, 24};
-    bitmap->pixels = (RGBTRIPLE *) calloc(1, size);
+  bitmap->fileHeader = (BITMAPFILEHEADER){BMP_MAGIC, headerSize + size, 0, 0, headerSize};
+  bitmap->dibHeader = (BITMAPCOREHEADER){12, width, height, 1, 24};
+  bitmap->pixels = (RGBTRIPLE *)calloc(1, size);
 
-    return bitmap;
+  return bitmap;
 }
 
 bool BitmapDestroy(Bitmap *bitmap) {
-    free(bitmap->pixels);
-    free(bitmap);
-    return true; // TODO: implement error handler
+  free(bitmap->pixels);
+  free(bitmap);
+  return true; // TODO: implement error handler
 }
 
 bool BitmapWriteFile(const Bitmap *bitmap, const char *filename) {
-    FILE *fp = fopen(filename, "w+");
-    fwrite(&bitmap->fileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
-    fwrite(&bitmap->dibHeader, sizeof(BITMAPCOREHEADER), 1, fp);
-    fwrite(bitmap->pixels, bitmap->fileHeader.bfSize - bitmap->fileHeader.bfOffBits, 1, fp);
-    fclose(fp);
-    return true; // TODO: implement error handler
+  FILE *fp = fopen(filename, "w+");
+  fwrite(&bitmap->fileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+  fwrite(&bitmap->dibHeader, sizeof(BITMAPCOREHEADER), 1, fp);
+  fwrite(bitmap->pixels, bitmap->fileHeader.bfSize - bitmap->fileHeader.bfOffBits, 1, fp);
+  fclose(fp);
+  return true; // TODO: implement error handler
 }
 
 bool BitmapSetPixelColor(Bitmap *bitmap, uint16_t x, uint16_t y, const RGBTRIPLE *color) {
-    if (bitmap->dibHeader.bcWidth <= x || bitmap->dibHeader.bcHeight <= y) {
-        fprintf(stderr, "%s: Invalid pixel indices (x:%d<%d, y:%d<%d)\n", __FUNCTION_NAME__, x, bitmap->dibHeader.bcWidth, y, bitmap->dibHeader.bcHeight);
-        return false;
-    }
-    bitmap->pixels[x + bitmap->dibHeader.bcWidth * (bitmap->dibHeader.bcHeight - y -1)] = *color;
-    return true;
+  if (bitmap->dibHeader.bcWidth <= x || bitmap->dibHeader.bcHeight <= y) {
+    fprintf(stderr, "%s: Invalid pixel indices (x:%d<%d, y:%d<%d)\n", __FUNCTION_NAME__, x, bitmap->dibHeader.bcWidth, y, bitmap->dibHeader.bcHeight);
+    return false;
+  }
+  bitmap->pixels[x + bitmap->dibHeader.bcWidth * (bitmap->dibHeader.bcHeight - y - 1)] = *color;
+  return true;
 }
--- bitmap.h
+++ bitmap.h
@@ -4,37 +4,38 @@
 #include "common.h"
 
 #define BMP_MAGIC 0x4d42
-#define BMP_COLOR(r, g, b) &(RGBTRIPLE){b, g, r}
+#define BMP_COLOR(r, g, b)                                                                                                                                     \
+  &(RGBTRIPLE) { b, g, r }
 
 #pragma pack(push)
 #pragma pack(1)
 typedef struct {
-    uint16_t bfType;
-    uint32_t bfSize;
-    uint16_t bfReserved1;
-    uint16_t bfReserved2;
-    uint32_t bfOffBits;
+  uint16_t bfType;
+  uint32_t bfSize;
+  uint16_t bfReserved1;
+  uint16_t bfReserved2;
+  uint32_t bfOffBits;
 } BITMAPFILEHEADER;
 
 typedef struct {
-    uint32_t bcSize;
-    uint16_t bcWidth;
-    uint16_t bcHeight;
-    uint16_t bcPlanes;
-    uint16_t bcBitCount;
+  uint32_t bcSize;
+  uint16_t bcWidth;
+  uint16_t bcHeight;
+  uint16_t bcPlanes;
+  uint16_t bcBitCount;
 } BITMAPCOREHEADER;
 
 typedef struct {
-    uint8_t rgbtBlue;
-    uint8_t rgbtGreen;
-    uint8_t rgbtRed;
+  uint8_t rgbtBlue;
+  uint8_t rgbtGreen;
+  uint8_t rgbtRed;
 } RGBTRIPLE;
 #pragma pack(pop)
 
 typedef struct {
-    BITMAPFILEHEADER fileHeader;
-    BITMAPCOREHEADER dibHeader;
-    RGBTRIPLE *pixels; // bottom-up
+  BITMAPFILEHEADER fileHeader;
+  BITMAPCOREHEADER dibHeader;
+  RGBTRIPLE *pixels; // bottom-up
 } Bitmap;
 
 Bitmap *BitmapNewImage(uint16_t width, uint16_t height);
@@ -42,4 +43,4 @@ bool BitmapDestroy(Bitmap *bitmap);
 bool BitmapWriteFile(const Bitmap *bitmap, const char *filename);
 bool BitmapSetPixelColor(Bitmap *bitmap, uint16_t x, uint16_t y, const RGBTRIPLE *color);
 
-#endif //RENDER_BITMAP_H
+#endif // RENDER_BITMAP_H
