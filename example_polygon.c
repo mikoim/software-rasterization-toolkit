42458615c2075873179041049dd3144fcb2735fc
Eshin Kunishima <ek@esh.ink>
2019-10-06 22:10:16 +0900

    add initial OpenMP support

--- example_polygon.c
+++ example_polygon.c
@@ -19,12 +19,6 @@ int main() {
   const int h = 1000;
   Bitmap *bmp = BitmapNewImage(w, h);
 
-  for (int y = 0; y < h; ++y) {
-    for (int x = 0; x < w; ++x) {
-      BitmapSetPixelColor(bmp, x, y, BMP_COLOR(0, 0, 0));
-    }
-  }
-
   Polygon *polygon = PolygonReadSTL("models/monkey.stl");
 
   Camera *camera = CameraPerspectiveProjection(V(1, 0, 0), V(0, 0, 0), V(0, 1, 0), w, h, 0.1, 1000, 120);
@@ -33,11 +27,17 @@ int main() {
   MatrixPrint(camera->camera2projection);
   MatrixPrint(camera->world2projection);
 
+#ifdef _OPENMP
+#pragma omp parallel for schedule(dynamic)
+#endif
   for (uint32_t i = 0; i < polygon->triangle; ++i) {
     Triangle triangle = rasterize(camera, polygon->triangles[i]);
     DrawTriangle(bmp, triangle.vertexes[0], triangle.vertexes[1], triangle.vertexes[2], BMP_COLOR(255, 0, 0));
   }
 
+#ifdef _OPENMP
+#pragma omp parallel for schedule(dynamic)
+#endif
   for (uint32_t i = 0; i < polygon->triangle; ++i) {
     Triangle triangle = rasterize(camera, polygon->triangles[i]);
     DrawLine(bmp, triangle.vertexes[0], triangle.vertexes[1], BMP_COLOR(0, 0, 255));
